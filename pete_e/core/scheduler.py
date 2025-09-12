df046a9ba07b9f610069057c4b7ae5f95a328f49
Ric <ric@brighteraccounting.com>
2025-09-12 06:10:13 +0100

    Add scheduler and infra utils stubs

---
+++ pete_e/core/scheduler.py
@@ -0,0 +1,7 @@
+""Session timing scheduler."""
+from datetime import time
+
+def assign_times(sessions: list) > list:
+    """Assign start/end times based on Blaze session position."""
+    # TODO: implement rules e.g. Blaze 6:15 -> weights at 7:00, Blaze >=7:00 -> weights 6:00
+    return sessions
